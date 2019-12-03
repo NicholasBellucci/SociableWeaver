506309bea9d81df657fad7c37b1f220994cd8021
NicholasBellucci <nickdbellucci@gmail.com>
2019-12-03 11:46:24 -0500

    Removed inits on structs

--- Sources/SociableWeaver/Helpers/Object.swift
+++ Sources/SociableWeaver/Helpers/Object.swift
@@ -8,15 +8,10 @@
 public struct Object {
     let name: String
     let result: String
-
-    private init(_ name: String, result: String) {
-        self.name = name
-        self.result = result
-    }
 }
 
 extension Object {
     init(_ name: String, @FieldsBuilder _ content: () -> String) {
-        self.init(name, result: content())
+        self.init(name: name, result: content())
     }
 }
--- Sources/SociableWeaver/Helpers/Weave.swift
+++ Sources/SociableWeaver/Helpers/Weave.swift
@@ -8,15 +8,10 @@
 public struct Weave {
     let type: OperationType
     var result: String
-
-    private init(_ type: OperationType, result: String) {
-        self.type = type
-        self.result = type.stringValue.withSubfields(result)
-    }
 }
 
 extension Weave {
     init(_ type: OperationType, @QueryBuilder _ content: () -> String) {
-        self.init(type, result: content())
+        self.init(type: type, result: type.stringValue.withSubfields(content()))
     }
 }
